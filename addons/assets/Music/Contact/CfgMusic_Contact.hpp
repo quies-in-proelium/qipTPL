f48abfc63fc5d3622ceba1bab7b7eb45a5b6bb7b
NemsisRE <nemesissre@gmail.com>
2019-08-28 22:01:51 +0000

    added contect ost

---
+++ addons/assets/Music/Contact/CfgMusic_Contact.hpp
@@ -0,0 +1,95 @@
+class Acclivity_Dawn {
+	name = "Acclivity Dawn";
+	sound[] = {QPATHTOF(Music\Contact\Acclivity_Dawn.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "59";
+	musicClass = "Contact";
+};
+
+class Anomalies {
+	name = "Anomalies";
+	sound[] = {QPATHTOF(Music\Contact\Anomalies.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "115";
+	musicClass = "Contact";
+};
+
+class Contact {
+	name = "Contact";
+	sound[] = {QPATHTOF(Music\Contact\Contact.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "205";
+	musicClass = "Contact";
+};
+
+class Infighting {
+	name = "Infighting";
+	sound[] = {QPATHTOF(Music\Contact\Infighting.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "208";
+	musicClass = "Contact";
+};
+
+class Night_Eyes_ZeroOne {
+	name = "Night Eyes Zero-One";
+	sound[] = {QPATHTOF(Music\Contact\Night_Eyes_ZeroOne.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "402";
+	musicClass = "Contact";
+};
+
+class Night_Eyes_ZeroTwo {
+	name = "Night Eyes Zero-Two";
+	sound[] = {QPATHTOF(Music\Contact\Night_Eyes_ZeroTwo.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "402";
+	musicClass = "Contact";
+};
+
+class Reveal {
+	name = "Reveal";
+	sound[] = {QPATHTOF(Music\Contact\Reveal.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = ""147"";
+	musicClass = "Contact";
+};
+
+class Sharper_Claws {
+	name = "Sharper Claws";
+	sound[] = {QPATHTOF(Music\Contact\Sharper_Claws.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "242";
+	musicClass = "Contact";
+};
+
+class Sunrise_ZeroOne {
+	name = "Sunrise Zero-One";
+	sound[] = {QPATHTOF(Music\Contact\Sunrise_ZeroOne.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "250";
+	musicClass = "Contact";
+};
+
+class Sunrise_ZeroTwo {
+	name = "Sunrise Zero-Two";
+	sound[] = {QPATHTOF(Music\Contact\Sunrise_ZeroTwo.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "247";
+	musicClass = "Contact";
+};
+
+class Transilience {
+	name = "Transilience";
+	sound[] = {QPATHTOF(Music\Contact\Transilience.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "244";
+	musicClass = "Contact";
+};
+
+class Wavering_Base {
+	name = "Wavering Base";
+	sound[] = {QPATHTOF(Music\Contact\Wavering.ogg), db+0, 1.0};
+	theme = "combat";
+	duration = "61";
+	musicClass = "Contact";
+};
