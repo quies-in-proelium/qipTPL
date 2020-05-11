1e2b72f612eced7c254f09dbb527cafca9eecb11
NemsisRE <nemesissre@gmail.com>
2020-05-11 16:22:37 +0000

    forgot some stuff

--- addons/equipment/CfgWeapons.hpp
+++ addons/equipment/CfgWeapons.hpp
@@ -1,31 +1,28 @@
 class CfgWeapons {
     class ACE_Vector;
-    class ACE_Vector_Designator: ACE_Vector {
+    class qipTPL_Vector_Designator: ACE_Vector {
         author = "NemesisRE";
         displayName = "[qip] ACE Vector 21 (Laser Designator)";
         descriptionShort = "Rangefinder/Designator";
-        simulation="weapon";
-		class WeaponSlotsInfo
-		{
-			mass=20;
-		};
-		Laser=1;
-		weaponInfoType="RscOptics_LaserDesignator";
-		visionMode[]=
-		{
-			"Normal",
-			"NVG",
-			"TI"
-		};
-		thermalMode[]={7};
-		opticsZoomMin=0.0099999998;
-		opticsZoomMax=0.050000001;
-		opticsZoomInit=0.050000001;
-		distanceZoomMin=100;
-		distanceZoomMax=2300;
-		minRange=150;
-		midRange=500;
-		maxRange=5000;
-        modelOptics = "\A3\Weapons_F\empty.p3d";
+        simulation = "weapon";
+        magazines[] = {"Laserbatteries"};
+        cursor = "laserDesignator";
+        cursorAim = "EmptyCursor";
+        cursorAimOn = "CursorAimOn";
+        showSwitchAction = 1;
+        class WeaponSlotsInfo {
+            mass = 20;
+        };
+        Laser = 1;
+        visionMode[] = {"Normal","NVG","TI"};
+        thermalMode[] = {0};
+        opticsZoomMin = 0.0099999998;
+        opticsZoomMax = 0.050000001;
+        opticsZoomInit = 0.050000001;
+        distanceZoomMin = 100;
+        distanceZoomMax = 2300;
+        minRange = 150;
+        midRange = 500;
+        maxRange = 5000;
     };
 };
